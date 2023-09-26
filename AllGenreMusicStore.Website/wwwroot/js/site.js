8d6817a0bf9d7e93c12eaa170e9de031005e0934
MonzurSiddique <mhsiddique@gmail.com>
2023-09-26 13:20:41 -0700

    Add project files.

---
+++ AllGenreMusicStore.Website/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Functions to be called onmousseover and onmouseout to change the color of "Welcome to All Genre Music Store"
+function newColor() {
+    document.getElementById("Welcome_to_All_Genre_Music_Store").style.color = "darkgreen";
+}
+
+function oldColor() {
+    document.getElementById("Welcome_to_All_Genre_Music_Store").style.color = "darkred";
+}
+
